2f5372470bd6f32f419fb5e910bc54a2a4c50dd3
keeepmin94 <wjdwkjh94@gmail.com>
2023-08-13 17:41:46 +0900

    add verifyToken

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -16,21 +16,29 @@ module.exports = {
       expiresIn: "1h", // 유효기간
     });
   },
-  verify: (token) => {
-    // access token 검증
-    let decoded = null;
+  verifyToken: (req, res, next) => {
     try {
-      decoded = jwt.verify(token, secret);
-      return {
-        ok: true,
-        id: decoded.id,
-        email: decoded.email,
-      };
-    } catch (err) {
-      return {
-        ok: false,
-        message: err.message,
-      };
+      console.log(req.headers);
+      // jwt의 검증이 끝나면 jwt의 내용물을 decoded에 넣는다
+      res.locals.decoded = jwt.verify(
+        //req.headers.authorization,
+        req.headers.authorization.split("Bearer ")[1], //postman 사용
+        process.env.SECRET
+      );
+      return next();
+    } catch (error) {
+      //유효 기간 초과
+      if (error.name === "TokenExpiredError") {
+        return res.status(419).json({
+          code: 419,
+          message: "토큰이 만료되었습니다.",
+        });
+      }
+      // 토큰이 위조(검증 실패)
+      return res.status(401).json({
+        code: 401,
+        message: "유효하지 않은 토큰입니다.",
+      });
     }
   },
 };
