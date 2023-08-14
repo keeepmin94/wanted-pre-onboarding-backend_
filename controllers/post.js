a611fbbc572970ef05f6a8c78af9cc9ac7d19dad
keeepmin94 <wjdwkjh94@gmail.com>
2023-08-14 16:01:03 +0900

    get post, patch post, delete post

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,6 @@
 const Post = require("../models/post");
+const User = require("../models/user");
+const counter = 5; //pagination 5개 고정
 
 exports.uploadPost = async (req, res, next) => {
   try {
@@ -13,3 +15,123 @@ exports.uploadPost = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getAllPosts = async (req, res, next) => {
+  try {
+    const params = req.params;
+    const posts = await Post.findAll(
+      {
+        attributes: ["id", "content", "createdAt", "updatedAt"],
+        include: { model: User, attributes: ["id", "email"] },
+      },
+      {
+        order: [["createdAt", "DESC"]],
+        offset: counter * (params.page - 1), //params.counter * params.page
+        limit: counter, //params.counter
+      }
+    );
+
+    res.json({
+      code: 200,
+      payload: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: "서버 에러",
+    });
+  }
+};
+
+exports.getPost = async (req, res, next) => {
+  try {
+    const params = req.params;
+    const post = await Post.findOne({
+      where: { id: params.id },
+      attributes: ["id", "content", "createdAt", "updatedAt"],
+      include: { model: User, attributes: ["id", "email"] },
+    });
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ errors: `Post [${params.id}]id doesn't exist.` });
+    }
+
+    res.json({
+      code: 200,
+      payload: post,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: "서버 에러",
+    });
+  }
+};
+
+exports.updatePost = async (req, res, next) => {
+  try {
+    const params = req.params;
+    const post = await Post.findOne({
+      where: { id: params.id },
+    });
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ errors: `Post [${params.id}]id doesn't exist.` });
+    }
+
+    if (post.UserId !== res.locals.decoded.id) {
+      return res.status(404).json({ errors: `Doesn't match the author's ID.` });
+    }
+
+    await post.update({ content: req.body.content });
+    await post.save();
+
+    res.json({
+      code: 200,
+      payload: post,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: "서버 에러",
+    });
+  }
+};
+
+exports.deletePost = async (req, res, next) => {
+    try {
+      const params = req.params;
+      const post = await Post.findOne({
+        where: { id: params.id },
+      });
+  
+      if (!post) {
+        return res
+          .status(404)
+          .json({ errors: `Post [${params.id}]id doesn't exist.` });
+      }
+  
+      if (post.UserId !== res.locals.decoded.id) {
+        return res.status(404).json({ errors: `Doesn't match the author's ID.` });
+      }
+  
+      await post.destroy();
+  
+      res.json({
+        code: 200,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        code: 500,
+        message: "서버 에러",
+      });
+    }
+  };
--- routes/post.js
+++ routes/post.js
@@ -5,12 +5,56 @@ const router = express.Router();
 const { validationResult } = require("express-validator");
 const {
   signupValidator,
+  paginationValidator,
+  postIdValidator,
   validatorErrorChecker,
 } = require("../middlewares/validators,js");
 const { verifyToken } = require("../middlewares/jwt");
-const { uploadPost } = require("../controllers/post");
+const {
+  uploadPost,
+  getAllPosts,
+  getPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/post");
 
-// POST /auth/join
+// POST /post/create
 router.post("/create", verifyToken, uploadPost);
+
+// GET /post/all/:page
+router.get(
+  "/all/:page",
+  verifyToken,
+  paginationValidator,
+  validatorErrorChecker,
+  getAllPosts
+); //"/:counter/:page"
+
+// GET /post/:id
+router.get(
+  "/:id",
+  verifyToken,
+  postIdValidator,
+  validatorErrorChecker,
+  getPost
+);
+
+// PATCH /post/:id
+router.patch(
+  "/:id",
+  verifyToken,
+  postIdValidator,
+  validatorErrorChecker,
+  updatePost
+);
+
+// DELETE /post/:id
+router.delete(
+  "/:id",
+  verifyToken,
+  postIdValidator,
+  validatorErrorChecker,
+  deletePost
+);
 
 module.exports = router;
